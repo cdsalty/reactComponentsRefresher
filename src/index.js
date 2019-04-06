9c3290afa90a1d03b167090cbe7b7801cb8b3f15
cdsalty <soltiscd@gmail.com>
2019-04-06 17:38:33 -0400

    Naive Component Approach; nd 2 fx styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi There</div>; 
+    return (
+        <div className="ui.container.comments d-block">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src="" alt="avatar" />
+                </a>
+                <div className = "content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 2:06PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+        
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// div className = "comment" represents one single comment by itself.
