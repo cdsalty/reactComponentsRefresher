0fa5d8b4de184330d6bc481d476483b7395b79a5
cdsalty <soltiscd@gmail.com>
2019-04-09 16:16:29 -0400

    updates to functional component

--- src/index.js
+++ src/index.js
@@ -2,34 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail
-              author = "Tom" 
-              timeAgo = "Today at 4:45PM" 
-              userContent = "Nice blog post" 
-              avatar = {faker.image.avatar()}
-              />
-           <CommentDetail 
-             author = "Sam" 
-             timeAgo = "Today at 2:00AM" 
-             userContent = "I don't understand."
-             avatar = {faker.image.avatar()}
-             />
-           <CommentDetail 
-             author = "Gus" 
-             timeAgo = "Yesterday at 5:00PM" 
-             userContent = "Great Info; Thanks for sharing"
-             avatar = {faker.image.avatar()}
-              />
-           <CommentDetail 
-             author = "Roy" 
-             timeAgo = "Yesterday at 1:30PM" 
-             userContent = "Thumbs up!"
-             avatar = {faker.image.avatar()}
-              />
+            <ApprovalCard />
+                <CommentDetail
+                author = "Tom" 
+                timeAgo = "Today at 4:45PM" 
+                userContent = "Nice blog post" 
+                avatar = {faker.image.avatar()}
+                />
+                <CommentDetail 
+                author = "Sam" 
+                timeAgo = "Today at 2:00AM" 
+                userContent = "I don't understand."
+                avatar = {faker.image.avatar()}
+                />
+                <CommentDetail 
+                author = "Gus" 
+                timeAgo = "Yesterday at 5:00PM" 
+                userContent = "Great Info; Thanks for sharing"
+                avatar = {faker.image.avatar()}
+                />
+                <CommentDetail 
+                author = "Roy" 
+                timeAgo = "Yesterday at 1:30PM" 
+                userContent = "Thumbs up!"
+                avatar = {faker.image.avatar()}
+                />
+            
         </div>
     )      
 };
