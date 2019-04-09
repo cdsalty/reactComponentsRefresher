ed46c04760626b4299e67041221b3b18024cce7c
cdsalty <soltiscd@gmail.com>
2019-04-09 12:56:42 -0400

    practice with passing data from parent to a child

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
+           <CommentDetail
+              author = "Tom" 
+              timeAgo = "Today at 4:45PM" 
+              userContent = "Nice blog post" 
+              avatar = {faker.image.avatar()}
+              />
+           <CommentDetail 
+             author = "Sam" 
+             timeAgo = "Today at 2:00AM" 
+             userContent = "I don't understand."
+             avatar = {faker.image.avatar()}
+             />
+           <CommentDetail 
+             author = "Gus" 
+             timeAgo = "Yesterday at 5:00PM" 
+             userContent = "Great Info; Thanks for sharing"
+             avatar = {faker.image.avatar()}
+              />
+           <CommentDetail 
+             author = "Roy" 
+             timeAgo = "Yesterday at 1:30PM" 
+             userContent = "Thumbs up!"
+             avatar = {faker.image.avatar()}
+              />
         </div>
     )      
 };
