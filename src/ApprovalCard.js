5a882f318acb9a9004cec72b618b33926d989fb0
cdsalty <soltiscd@gmail.com>
2019-04-10 23:22:58 -0400

    showing children to component

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+  // console.log(props);
+  console.log(props.children); //This shows us the react component itself so we know we can call on it below.
     return (
       <div className = "ui card">
-        <div className = "content">Are you sure?</div>
+        <div className = "content">{props.children}</div>
         <div className = "extra content">
           <div className = "ui two buttons">
             <div className = "ui basic green button">Approve</div>
@@ -15,3 +17,12 @@ const ApprovalCard = () => {
 };
 
 export default ApprovalCard;
+
+
+/*
+replaced...
+<div className = "content">Are you sure?</div>
+with:
+<div className = "content">{props.children}</div>
+because we found this in our console.log on the functional component, itself.
+*/
--- src/index.js
+++ src/index.js
@@ -7,31 +7,38 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
+            <ApprovalCard>
                 <CommentDetail
                 author = "Tom" 
                 timeAgo = "Today at 4:45PM" 
                 userContent = "Nice blog post" 
                 avatar = {faker.image.avatar()}
                 />
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail 
                 author = "Sam" 
                 timeAgo = "Today at 2:00AM" 
                 userContent = "I don't understand."
                 avatar = {faker.image.avatar()}
                 />
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail 
                 author = "Gus" 
                 timeAgo = "Yesterday at 5:00PM" 
                 userContent = "Great Info; Thanks for sharing"
                 avatar = {faker.image.avatar()}
                 />
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail 
                 author = "Roy" 
                 timeAgo = "Yesterday at 1:30PM" 
                 userContent = "Thumbs up!"
                 avatar = {faker.image.avatar()}
                 />
+            </ApprovalCard>
             
         </div>
     )      
